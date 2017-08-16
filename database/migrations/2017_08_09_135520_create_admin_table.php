5d16175d1493745f90a178fea0db26deae1c43cc
Ian Too <iantoo@icloud.com>
2017-08-16 15:56:43 +0300

    Added the correct create_admin_table_migration

--- database/migrations/2017_08_09_135520_create_admin_table.php
+++ database/migrations/2017_08_09_135520_create_admin_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AdminTable extends Migration
+class CreateAdminTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,13 @@ class AdminTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +29,6 @@ class AdminTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('admin');
     }
 }
