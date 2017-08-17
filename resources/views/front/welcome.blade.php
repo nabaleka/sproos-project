fc555be881eb5f5e80edd1a88eb104589742aeee
Ian Too <iantoo@icloud.com>
2017-08-17 14:41:12 +0300

    App template

--- resources/views/front/welcome.blade.php
+++ resources/views/front/welcome.blade.php
@@ -139,15 +139,14 @@
       <section class="container padding-top-3x padding-bottom-3x">
         <h3 class="text-center mb-30">Featured Products</h3>
         <div class="owl-carousel" data-owl-carousel="{ &quot;nav&quot;: false, &quot;dots&quot;: true, &quot;margin&quot;: 30, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:1},&quot;576&quot;:{&quot;items&quot;:2},&quot;768&quot;:{&quot;items&quot;:3},&quot;991&quot;:{&quot;items&quot;:4},&quot;1200&quot;:{&quot;items&quot;:4}} }">
-
-        
+      @foreach($products as $product)
         <!-- Loop through -->
           <div class="grid-item">
             <div class="product-card">
-              <div class="product-badge text-danger">22% Off</div><a class="product-thumb" href="shop-single.html"></a>
+              <div class="product-badge text-danger">22% Off</div><a class="product-thumb" href="/shop-single{{$product->id}}"><img src="/storage/products/{{$product->image}}" height="100px" width="150px" /></a>
               <h3 class="product-title"><a href="shop-single/["></a></h3>
               <h4 class="product-price">
-                <del>Ksh4445.95</del>
+                <del>{{$product->price}}</del>
               </h4>
               <div class="product-buttons">
                 <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist"><i class="icon-heart"></i></button>
@@ -155,6 +154,7 @@
               </div>
             </div>
           </div>
+          @endforeach
 
           <!-- Product-->
           @endif
