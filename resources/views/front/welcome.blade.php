1f11f9cc8ca0b8b651de578265551446698bb4bc
254Orao <otobiasorao@gmail.com>
2017-08-17 10:38:15 +0300

    Welcome page

--- resources/views/front/welcome.blade.php
+++ resources/views/front/welcome.blade.php
@@ -137,12 +137,13 @@
         <h3 class="text-center mb-30">Featured Products</h3>
         <div class="owl-carousel" data-owl-carousel="{ &quot;nav&quot;: false, &quot;dots&quot;: true, &quot;margin&quot;: 30, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:1},&quot;576&quot;:{&quot;items&quot;:2},&quot;768&quot;:{&quot;items&quot;:3},&quot;991&quot;:{&quot;items&quot;:4},&quot;1200&quot;:{&quot;items&quot;:4}} }">
           <!-- Product-->
+          @foreach ($products as $product)
           <div class="grid-item">
             <div class="product-card">
-              <div class="product-badge text-danger">22% Off</div><a class="product-thumb" href="shop-single.html"></a>
-              <h3 class="product-title"><a href="shop-single.html"></a></h3>
+              <div class="product-badge text-danger">22% Off</div><a class="product-thumb" href="shop-single.html"><img src="{{Storage::disk('local')->url($product->image)}}" height="100px" width="150px" /></a>
+              <h3 class="product-title"><a href="shop-single.html">{{ $product->name }}</a></h3>
               <h4 class="product-price">
-                <del>Ksh4445.95</del>
+                <del>Ksh4445.95</del>{{ $product->price }}
               </h4>
               <div class="product-buttons">
                 <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist"><i class="icon-heart"></i></button>
@@ -150,7 +151,8 @@
               </div>
             </div>
           </div>
-          
+          <!-- Product-->
+          @endforeach
           
         </div>
       </section>
