b84cbf3543e42a6a90b59acf2ef4663b37b351b4
Ian Too <iantoo@icloud.com>
2017-08-29 09:57:02 +0300

    Admin controller funcions. Manage

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -7,5 +7,32 @@ use App\Http\Controllers\Controller;
 
 class AdminController extends Controller
 {
-    //
+    /* Restricts access to only the admin */
+    public function __construct()
+	{
+	    $this->middleware('auth:admin');
+	}
+	
+    /*
+    Controll actions of the admin
+    */
+
+    public function manage(){
+        #this is the first page that will appear
+        #shows various options in summary:
+        # - Featured products
+        # - top banner
+        # - categories
+        # - top sellers
+    }
+
+    # Featured page
+
+    # categories
+
+    #top sellers
+
+    # banner
+
+
 }
