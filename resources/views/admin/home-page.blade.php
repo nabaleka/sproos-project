666c3e0876a6177964e3aca8faebf79b74f5c689
Ian Too <iantoo@icloud.com>
2017-08-29 14:00:23 +0300

    Changed the main sidebar. Few chages.

--- resources/views/admin/home-page.blade.php
+++ resources/views/admin/home-page.blade.php
@@ -18,6 +18,70 @@
 
   <!-- Main content -->
     <section class="content">
+<!-- Home page info -->
+     <!-- Info boxes -->
+      <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-aqua"><i class="fa fa-users"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Banners</span>
+              <span class="info-box-number">2</span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-red"><i class="fa fa-bar-chart"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Featured Products</span>
+              <span class="info-box-number">10</span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+                <!-- fix for small devices only -->
+        <div class="clearfix visible-sm-block"></div>
+
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-green"><i class="fa fa-envelope"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Top Sellers</span>
+              <span class="info-box-number"> 10</span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-yellow"><i class="fa fa-flag"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Categories present</span>
+              <span class="info-box-number">2,000</span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+
+<!-- -->
+
+
 
       <!-- Default box -->
       <div class="box">
@@ -57,8 +121,11 @@
       </h4>
     </div>
     <div id="collapse2" class="panel-collapse collapse">
-      <div class="panel-body">You can feature a product here. Earn from this.</div>
-      <button class="btn btn-pimary">Add</button>
+      <div class="panel-body">You can feature a product here. Earn from this.
+        <div>
+            <button class="btn btn-success">Add</button>
+        </div>
+    </div>
     </div>
   </div>
   <div class="panel panel-default">
@@ -69,8 +136,12 @@
       </h4>
     </div>
     <div id="collapse3" class="panel-collapse collapse">
-      <div class="panel-body">Here, add top sellers bases on sales</div><!-- Panel body-->
-      <button class="btn btn-pimary">Add</button>
+      <div class="panel-body">Here, add top sellers bases on sales
+        <div>
+            <button class="btn btn-success">Add</button>
+        </div>
+      </div><!-- Panel body-->
+      
     </div> <!--Collapse panel -->
   </div>
 
@@ -82,8 +153,11 @@
       </h4>
     </div>
     <div id="collapse4" class="panel-collapse collapse">
-      <div class="panel-body">Manage categories that will show up in your homepage</div><!-- Panel body-->
-      <a class="btn btn-pimary" href="/admin/category/create">Add</a>
+      <div class="panel-body">Manage categories that will show up in your homepage
+    <div>
+        <button class="btn btn-success">Add</button>
+    </div>
+      </div><!-- Panel body-->
     </div> <!--Collapse panel -->
   </div>
 </div> 
