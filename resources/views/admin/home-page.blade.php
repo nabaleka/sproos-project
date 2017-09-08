28641315bf2c5591488e24322d74cd15712e205e
Ian Too <iantoo@icloud.com>
2017-09-08 11:28:47 +0300

    Sepatrated stats widgets into a component

--- resources/views/admin/home-page.blade.php
+++ resources/views/admin/home-page.blade.php
@@ -12,74 +12,8 @@
       <li class="active">Home Pages</li>
     </ol>
   </section>
-
   <!-- Main content -->
     <section class="content">
-<!-- Home page info -->
-     <!-- Info boxes -->
-      <div class="row">
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-aqua"><i class="fa fa-users"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Banners</span>
-              <span class="info-box-number">2</span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-red"><i class="fa fa-bar-chart"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Featured Products</span>
-              <span class="info-box-number">10</span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-                <!-- fix for small devices only -->
-        <div class="clearfix visible-sm-block"></div>
-
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-green"><i class="fa fa-envelope"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Top Sellers</span>
-              <span class="info-box-number"> 10</span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-yellow"><i class="fa fa-flag"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Categories present</span>
-              <span class="info-box-number">2,000</span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-      </div>
-      <!-- /.row -->
-
-<!-- -->
-
-
-
       <!-- Default box -->
       <div class="box">
         <div class="box-header with-border">
