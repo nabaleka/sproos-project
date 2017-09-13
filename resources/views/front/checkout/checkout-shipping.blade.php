76edcf1b937388a4c367735682fe8a6e2747e70d
254Orao <otobiasorao@gmail.com>
2017-09-13 16:26:48 +0300

    checkout ship

--- resources/views/front/checkout/checkout-shipping.blade.php
+++ resources/views/front/checkout/checkout-shipping.blade.php
@@ -55,7 +55,6 @@
 
     <hr class="padding-bottom-1x">
 
-<<<<<<< HEAD
     <form>
     <div class="row">
         <div class="col-sm-6">
@@ -77,96 +76,6 @@
         </label></div>
         </div>
         </div>
-=======
-    <div class="table-responsive">
-
-        <table class="table">
-
-        <thead class="thead-default">
-
-            <tr>
-
-            <th>Shipping method</th>
-
-            <th>--</th>
-
-            <th>--</th>
-
-            <th></th>
-
-            </tr>
-
-        </thead>
-
-        <tbody>
-
-           
-
-            <tr>
-
-            <td class="align-middle"><span class="text-medium">
-        <div id="locationField">
-      <input id="autocomplete" placeholder="Enter your address"
-             onFocus="geolocate()" type="text"></input>
-    </div>
-
-    <table id="address">
-      <tr>
-        <td class="label">Street address</td>
-        <td class="slimField"><input class="field" id="street_number"
-              disabled="true"></input></td>
-        <td class="wideField" colspan="2"><input class="field" id="route"
-              disabled="true"></input></td>
-      </tr>
-      <tr>
-        <td class="label">City</td>
-        <!-- Note: Selection of address components in this example is typical.
-             You may need to adjust it for the locations relevant to your app. See
-             https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-addressform
-        -->
-        <td class="wideField" colspan="3"><input class="field" id="locality"
-              disabled="true"></input></td>
-      </tr>
-      <tr>
-        <td class="label">State</td>
-        <td class="slimField"><input class="field"
-              id="administrative_area_level_1" disabled="true"></input></td>
-        <td class="label">Zip code</td>
-        <td class="wideField"><input class="field" id="postal_code"
-              disabled="true"></input></td>
-      </tr>
-      <tr>
-        <td class="label">Country</td>
-        <td class="wideField" colspan="3"><input class="field"
-              id="country" disabled="true"></input></td>
-      </tr>
-    </table>
-
-            </span><br><span class="text-muted text-sm">East Africa Only!</span></td>
-
-            <td class="align-middle">&mdash;</td>
-
-            <td class="align-middle">&mdash;</td>
-
-            <td class="align-middle">
-
-                <label class="custom-control custom-radio">
-
-                <input class="custom-control-input" type="radio" name="shipping" value="2,400.00" ><span class="custom-control-indicator"></span>
-
-                </label>
-
-            </td>
-
-            </tr>
-
-            
-
-        </tbody>
-
-        </table>
-
->>>>>>> 0bce14981290e701b0dd50f8ceb91b94b0911c47
     </div>
     <div class="row">
         <div class="col-sm-6">
