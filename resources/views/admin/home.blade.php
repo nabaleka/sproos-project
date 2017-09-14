34ed47b19487bdca29093accec7149bfb95c1b1c
254Orao <otobiasorao@gmail.com>
2017-09-14 11:19:31 +0300

    seller

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -30,7 +30,7 @@
         <!-- /.col -->
         <div class="col-md-3 col-sm-6 col-xs-12">
           <div class="info-box">
-            <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
+            <span class="info-box-icon bg-red"><i class="fa fa-"></i></span>
 
             <div class="info-box-content">
               <span class="info-box-text">Sellers</span>
