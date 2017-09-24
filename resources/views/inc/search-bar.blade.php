08d00ec08fc5b70229912da27cd390b875e1075c
Ian Too <iantoo@icloud.com>
2017-09-24 19:24:57 +0300

    Fixed search and shows seller of the product

--- resources/views/inc/search-bar.blade.php
+++ resources/views/inc/search-bar.blade.php
@@ -5,7 +5,8 @@
                 <form class="input-group form-group " style="margin-bottom:0;" method="post" action="/hi"><span class="input-group-btn" >
                 {{ csrf_field()}}
                     <button type="submit" class="btn btn-primary" style="color:#ee2956;"><i class="icon-search"></i></button></span>
-                  <input class="form-control" style="border:unset; border-bottom:1px ;" type="search" placeholder="Search by Category, Price, Product Name">
+                  <input class="form-control" style="border:unset; border-bottom:1px ;" type="search" placeholder="Search by Category, Price,iji Product Name">
+                  
                 </form>
             </div>
         </li>
